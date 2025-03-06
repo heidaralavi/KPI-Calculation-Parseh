4003576076c4359b6174a336d9bba0d544a38cbe
heidar alavi <alavi.h@gmail.com>
2025-03-06 09:37:11 +0330

    update

--- emrs.py
+++ emrs.py
@@ -2,6 +2,7 @@ import pandas as pd
 import jdatetime
 import re
 
+# function for jalalli datetime
 def to_jalali(x):
     if isinstance(x, str):
         m = re.search(r"\b(14\d\d)[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12]\d|3[01])\b", x)
@@ -12,54 +13,76 @@ def to_jalali(x):
     return None
 #print(to_jalali('1403/01/02'))
 
+# reading parseh output results
 emrs_parseh = pd.read_excel('شاخص EMRS (سورت شده).xlsx',header=None)
-emrs_parseh[0] = pd.to_numeric(emrs_parseh[0], errors='coerce')
-emrs_parseh = emrs_parseh[emrs_parseh[0].notna()]
 
+# filltering empty columns and cells
+emrs_parseh[0] = pd.to_numeric(emrs_parseh[0], errors='coerce')*100
+emrs_parseh = emrs_parseh[emrs_parseh[0].notna()]
 emrs_parseh.drop(labels=[2,3,5,7,8,9,10,12,13],inplace=True,axis=1)
 emrs_parseh.reset_index(inplace=True,drop=True)
+columns_name = {0:'EMRS',1:'تعداد دستورکارهای EM',4:'تعداد EMهای اتمام یافته کمتراز ۳روز',6:'AppTag تجهیز',11:'نام تجهیز',14:'ردیف'}
+emrs_parseh.rename(columns=columns_name,inplace=True)
+#saving parseh output to excell
 emrs_parseh.to_excel('parseh.xlsx',index=False)
 #print(emrs_parseh.tail(20))
 
+#reading all workorder from parseh
 dastorkar = pd.read_excel('تاریخچه دستور کارها.xlsx')
-print(dastorkar.shape)
+#print(dastorkar.shape)
+
+#filltering data
 dastorkar['تاریخ شروع'] = dastorkar['تاریخ شروع'].apply(to_jalali)
 dastorkar['تاریخ اتمام'] = dastorkar['تاریخ اتمام'].apply(to_jalali)
 from_date = jdatetime.date(year=1403,month=5,day=1)
 to_date = jdatetime.date(year=1403,month=11,day=1)
-mask_1 = dastorkar['تاریخ شروع'].between(from_date,to_date)
+
+mask_1 = dastorkar['وضعیت'].eq('اتمام یافته')
+#print(mask_1)
 dastorkar = dastorkar[mask_1]
 mask_2 = dastorkar['نوع فعالیت'].eq('اضطراری')
-print(mask_2)
+#print(mask_2)
 dastorkar = dastorkar[mask_2]
+mask_3 = dastorkar['تاریخ شروع'].between(from_date,to_date)
+dastorkar = dastorkar[mask_3]
 #print(dastorkar.head())
+
+#calculate task durations
 delta = dastorkar['تاریخ اتمام'] - dastorkar['تاریخ شروع']
 delta_days = []
-log3 = []
+smaler_than_3 = []
 for item in delta:
     #print(type(item))
     if isinstance(item, jdatetime.timedelta):
         if item.days <=3 :
-            log3.append(1)
+            smaler_than_3.append(1)
         else:
-            log3.append(0)
+            smaler_than_3.append(0)
         delta_days.append(item.days)
         #print(item.days)
     else:
         delta_days.append(None)
-        log3.append(None)
+        smaler_than_3.append(None)
 
 #print(len(delta_days))
+
+#add to dataframe
 dastorkar['delta'] = delta_days
-dastorkar['LOG3'] = log3
+dastorkar['smaler_than_3'] = smaler_than_3
 
+#calculating
+temp1 = dastorkar.groupby('AppTAG')['smaler_than_3'].sum()
+temp2 = dastorkar.groupby('AppTAG')['شماره دستور کار'].nunique()
+temp = pd.merge(temp1,temp2,on='AppTAG')
+temp['emrs'] = round(100.0*temp['smaler_than_3']/temp['شماره دستور کار'],2)
+temp.sort_values(by='emrs',ascending=True,inplace=True)
+#print(temp.head())
 
-test1 = dastorkar.groupby('AppTAG')['LOG3'].sum()
-test2 = dastorkar.groupby('AppTAG')['شماره دستور کار'].nunique()
-test3 = pd.merge(test1,test2,on='AppTAG')
-test3['emrs'] = round(100.0*test3['LOG3']/test3['شماره دستور کار'],2)
-test3.sort_values(by='emrs',ascending=True,inplace=True)
-print(test3.head())
-test3.to_excel('check_point.xlsx')
+#rename columns
+columns_newname = {'شماره دستور کار':'تعداد دستورکارهای اضطراری'}
+temp.rename(columns=columns_newname,inplace=True)
+#saving
+temp.to_excel('check_point.xlsx')
+
 #dastorkar.to_excel('temp_t.xlsx')
 
